07b4c99e705e8d412835bed4618bc1ee497886fe
Airxel <airxeldroide@gmail.com>
2025-01-10 09:48:02 +0000

    Día 1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public InputAction MoveAction;
+
+    private void Start()
+    {
+        MoveAction.Enable();
+    }
+
+    private void Update()
+    {
+        Vector2 playerMovement = MoveAction.ReadValue<Vector2>();
+        Debug.Log(playerMovement);
+
+        Vector2 playerPosition = (Vector2)transform.position + playerMovement * 0.05f;
+
+        this.transform.position = playerPosition;
+    }
+}
