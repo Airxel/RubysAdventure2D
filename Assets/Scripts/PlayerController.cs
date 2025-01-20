8fa1c8fcdd3b418801104311334eb4b4d7a4f7e8
Airxel <airxeldroide@gmail.com>
2025-01-20 22:06:09 +0000

    Día 4
    Some comments

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,21 +5,25 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    // Variables referentes al movimiento del jugador
     public InputAction MoveAction;
 
     private Rigidbody2D playerRb;
     private Vector2 playerMovement;
     public float speed = 5.0f;
 
+    // Variables referentes al sistema de vidas
     public int maxHealth = 5;
 
     public int health { get { return currentHealth; } }
     private int currentHealth;
 
+    // Variables referentes al tiempo de invulnerabilidad
     public float timeInvincible = 2.0f;
     private bool isInvincible;
     private float damageCooldown;
 
+    // Variables referentes a la zona de regeneración
     public float timeHealing = 5.0f;
     private bool isHealing;
     private float healingCooldown;
